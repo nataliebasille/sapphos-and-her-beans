fc6ec890fbaa8eb8236bca71ef1a8a0a85efa1ae
Natalie Basille <natalie.basille1@gmail.com>
2024-07-27 10:27:46 -0400

    Move common db code from admin to root

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,8 +1,12 @@
+const dotenv = await import('dotenv');
+
+dotenv.config({path: '../.env'})
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import("../env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {};
