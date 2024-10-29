435118b866e933dc477b11eac19f7e2743b166b6
Natalie Basille <natalie.basille1@gmail.com>
2024-10-29 14:57:38 -0400

    Add blur on scroll to header for home page

--- ecommerce/src/app/shop-header.tsx
+++ ecommerce/src/app/shop-header.tsx
@@ -6,6 +6,7 @@ import { useCartIsDisabled, useOpenCart } from "./_stores/cart";
 import { NavMenu } from "./_components/nav-menu";
 import { CartIcon } from "./_components/cart-icon";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export const ShopHeader = () => {
   const path = usePathname();
@@ -14,12 +15,28 @@ export const ShopHeader = () => {
   const contrast = isHome ? "white" : "black";
   const openCart = useOpenCart();
   const isDisabled = useCartIsDisabled();
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener("scroll", onScroll);
+
+    return () => {
+      // Remove the event listener when the component is unmounted
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, []);
 
   return (
     <header
       className={twMerge(
-        "fixed top-0 z-50 flex w-full items-center border-b-[1px] py-4",
-        path !== "/" && "bg-[#F7DCDF]",
+        "fixed top-0 z-50 flex w-full items-center border-b-[1px] py-4 backdrop-blur-xl backdrop-opacity-0 transition-[backdrop-filter] duration-200",
+        !isHome && "bg-[#F7DCDF]",
+        scrolled && "backdrop-opacity-100",
         contrast === "white" ?
           "border-slate-200/30 text-white"
         : "border-slate-800/30 text-black",
