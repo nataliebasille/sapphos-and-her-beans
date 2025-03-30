47e015945f6c322d64ab9ada9a4d95d0ad65f433
Natalie Basille <natalie.basille1@gmail.com>
2025-03-30 09:51:58 -0400

    update nextjs

---
+++ apps/ecommerce/src/app/wholesale/page.tsx
@@ -0,0 +1,31 @@
+import { twMerge } from "tailwind-merge";
+import { Heading } from "../_components/heading";
+import { useProductList } from "../_stores/products";
+import { ProductCard } from "../_components/product-card";
+
+export default function WholesalesPage() {
+  const products = useProductList();
+
+  return (
+    <div className="px-4 md:px-10">
+      <div
+        className={twMerge(
+          "mb-5 border-b-[1px] border-black/30 md:col-span-2 md:mb-10",
+        )}
+      >
+        <Heading
+          level={3}
+          className="mb-0 text-center uppercase tracking-wide md:text-left"
+        >
+          Wholesale
+        </Heading>
+      </div>
+
+      {/* <div className="grid gap-4 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+        {products.map((product, index) => (
+          <ProductCard key={index} {...product} />
+        ))}
+      </div> */}
+    </div>
+  );
+}
